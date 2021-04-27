b7f4f608543a3c52cdd98c5e793075f21b6c5851
cnstrct0x <cnstrct0x@gmail.com>
2021-04-27 16:08:16 +0200

    :sparkles: favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,20 @@ const LandingPage = () => {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
+
+        {'<!-- Favicon -->'}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <Header />
       <Layout.Content>
