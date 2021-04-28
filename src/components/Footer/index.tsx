734a8a9bf0c8bd3343ccd408bc5892fb9d10a84a
cnstrct0x <cnstrct0x@gmail.com>
2021-04-28 11:45:18 +0200

    :sparkles: Proof of burn

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,39 +1,60 @@
+import ProofOfBurn from 'components/ProofOfBurn';
+import { MouseEvent, useCallback, useState } from 'react';
+
 import styles from './styles.module.css';
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <nav>
-      <a href="whitepaper.pdf" target="_blank" rel="noreferrer">
-        Whitepaper
-      </a>
-      <a
-        href="https://bscscan.com/token/0x9685208eb8Daf44578c0da4257A5f0E5a6ef97e6"
-        target="_blank"
-        rel="noreferrer"
-      >
-        BscScan
-      </a>
-    </nav>
-    <nav>
-      <a
-        href="https://twitter.com/lambrofinance"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Twitter
-      </a>
-      <a href="https://t.me/lambrofinance" target="_blank" rel="noreferrer">
-        Telegram
-      </a>
-      <a
-        href="https://github.com/lambrofinance"
-        target="_blank"
-        rel="noreferrer"
-      >
-        GitHub
-      </a>
-    </nav>
-  </footer>
-);
+const Footer = () => {
+  const [proofOfBurnVisible, setProofOfBurnVisible] = useState(false);
+
+  const onBurnClick = useCallback((e: MouseEvent<unknown>) => {
+    e.preventDefault();
+
+    setProofOfBurnVisible(true);
+  }, []);
+
+  const onHide = useCallback(() => {
+    setProofOfBurnVisible(false);
+  }, []);
+
+  return (
+    <>
+      {proofOfBurnVisible && <ProofOfBurn onHide={onHide} />}
+      <footer className={styles.footer}>
+        <nav>
+          <a href="whitepaper.pdf" target="_blank" rel="noreferrer">
+            Whitepaper
+          </a>
+          <a
+            href="https://bscscan.com/token/0x9685208eb8Daf44578c0da4257A5f0E5a6ef97e6"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Token
+          </a>
+          <a onClick={onBurnClick}>Burn</a>
+        </nav>
+        <nav>
+          <a
+            href="https://twitter.com/lambrofinance"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
+          </a>
+          <a href="https://t.me/lambrofinance" target="_blank" rel="noreferrer">
+            Telegram
+          </a>
+          <a
+            href="https://github.com/lambrofinance"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </nav>
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
---
+++ src/components/ProofOfBurn/index.tsx
@@ -0,0 +1,80 @@
+import { MouseEvent, useCallback, useRef } from 'react';
+
+import styles from './styles.module.css';
+
+interface Props {
+  onHide: () => void;
+}
+
+const ProofOfBurn = (props: Props) => {
+  const { onHide } = props;
+  const backdrop = useRef(null);
+
+  const onBackdropClick = useCallback(
+    (e: MouseEvent<unknown>) => {
+      if (e.target === backdrop.current) {
+        onHide();
+      }
+    },
+    [backdrop, onHide],
+  );
+
+  return (
+    <div
+      className={styles.proofOfBurn}
+      onClick={onBackdropClick}
+      ref={backdrop}
+    >
+      <div>
+        <h2>Contract creation &amp; proof of burn</h2>
+
+        <h3>Token</h3>
+        <a
+          href="https://bscscan.com/token/0x9685208eb8daf44578c0da4257a5f0e5a6ef97e6"
+          target="_blank"
+          rel="noreferrer"
+        >
+          0x9685208eb8daf44578c0da4257a5f0e5a6ef97e6
+        </a>
+
+        <h3>Contract creation &amp; mint</h3>
+        <a
+          href="https://bscscan.com/tx/0xe137c92122b0234da1222d5e5bae2a3e395214b14aa29db84f1e1f332170363c"
+          target="_blank"
+          rel="noreferrer"
+        >
+          0xe137c92122b0234da1222d5e5bae2a3e395214b14aa29db84f1e1f332170363c
+        </a>
+
+        <h3>PCS BNB:LAMBRO pool creation</h3>
+        <a
+          href="https://bscscan.com/tx/0xcdc94ebd7a1fcd17cec7a7419e4e57236d5df46b5427677776b5391778328911"
+          target="_blank"
+          rel="noreferrer"
+        >
+          0xcdc94ebd7a1fcd17cec7a7419e4e57236d5df46b5427677776b5391778328911
+        </a>
+
+        <h3>Burn PCS BNB:LAMBRO LP redemption tokens</h3>
+        <a
+          href="https://bscscan.com/tx/0xaf24dfab44d64ed8328de64a92a9fbe28080a752961d7e9d522b78e1a0a7b1b4"
+          target="_blank"
+          rel="noreferrer"
+        >
+          0xaf24dfab44d64ed8328de64a92a9fbe28080a752961d7e9d522b78e1a0a7b1b4
+        </a>
+
+        <h3>Burn contract owner / admin keys</h3>
+        <a
+          href="https://bscscan.com/tx/0xc3771bca578cd9f91dfb312a1484dca452c5e30915eb26277aaf058ee6774a8f"
+          target="_blank"
+          rel="noreferrer"
+        >
+          0xc3771bca578cd9f91dfb312a1484dca452c5e30915eb26277aaf058ee6774a8f
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProofOfBurn;
